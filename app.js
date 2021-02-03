582716a66c119e6359da02ca589426ba17c842f8
wenchaoyue <584330116@qq.com>
2021-02-03 18:25:49 +0800

    02-03

---
+++ app.js
@@ -0,0 +1,7 @@
+/*
+ * @Author: 
+ * @Date: 2021-02-03 15:30:20
+ * @LastEditors: Chaoyue
+ * @LastEditTime: 2021-02-03 15:30:21
+ * @FilePath: \canvas-demo\app.js
+ */
--- my-canvas.js
+++ my-canvas.js
@@ -2,7 +2,7 @@
  * @Author: 
  * @Date: 2021-02-02 17:56:53
  * @LastEditors: Chaoyue
- * @LastEditTime: 2021-02-02 18:16:47
+ * @LastEditTime: 2021-02-03 15:19:09
  * @FilePath: \canvas-demo\my-canvas.js
  */
 class CanvasImg {
@@ -18,12 +18,12 @@ class CanvasImg {
         this.img = new Image();
         this.img.src = src;
         this.particleList = [];
-        this.cell_w = 4;
-        this.cell_h = 4;
+        this.cell_w = 3;
+        this.cell_h = 3;
         this.cols = parseInt(this.width / this.cell_w);
         this.rows = parseInt(this.height / this.cell_h);
         this.count = 0;
-        this.animateTime = 280;
+        this.animateTime = 400;
         this.frameId = '';
         this.init();
     }
@@ -33,7 +33,7 @@ class CanvasImg {
         this.img.onload = () => {
             this.ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
             this.imageData = this.ctx.getImageData(this.x, this.y, this.width, this.height).data;
-            console.log(this.imageData);
+            console.log(this.imageData.slice(0, 1000));
             this.caculate();
             this.step();
         }
@@ -46,36 +46,46 @@ class CanvasImg {
                 let s_x = Math.floor(Math.random() * this.width * 1.5);
                 let s_y = Math.floor(Math.random() * this.height * 1.5);
                 s_x = this.width / 2;
-                s_y = (this.height + Math.random() * this.height * 10);
-                this.particleList.push(new Particle(x, y, this.cell_w, this.cell_h, cell_index, this.imageData,
-                    (this.x + s_x), (this.y + s_y),
-                    (this.x + x * this.cell_w), (this.y + y * this.cell_h)));
-                if (x < 2 && y < 1) {
-                    console.log(this.imageData.slice(0, cell_index));
-                    console.log(cell_index);
+                s_y = this.height + 2;
+                // s_y = (this.height + Math.random() * this.height * 10);
+                if (this.imageData[cell_index + 3] > 50) {
+                    this.particleList.push(new Particle(x, y, this.cell_w, this.cell_h, cell_index, this.imageData,
+                        (this.x + s_x), (this.y + s_y),
+                        (this.x + x * this.cell_w), (this.y + y * this.cell_h)));
                 }
             }
         }
+        console.log(this.particleList.slice(0, 10));
+        console.log(this.particleList.length);
 
     }
     drawImg() {
-        this.count++;
         this.ctx.clearRect(this.x, this.y, this.width, this.height);
         for (let i = 0; i < this.particleList.length; i++) {
+            let r = Math.random()
             this.ctx.fillStyle = this.particleList[i].style;
-            this.particleList[i].c_x = this.easeInOutExpo(this.count, this.particleList[i].s_x, this
+            this.particleList[i].c_t++;
+            this.particleList[i].c_x = this.easeInOutExpo(this.particleList[i].c_t, this.particleList[i].s_x, this
                 .particleList[i].e_x - this.particleList[i].s_x, this.animateTime);
-            this.particleList[i].c_y = this.easeInOutExpo(this.count, this.particleList[i].s_y, this
+            this.particleList[i].c_y = this.easeInOutExpo(this.particleList[i].c_t, this.particleList[i].s_y, this
                 .particleList[i].e_y - this.particleList[i].s_y, this.animateTime);
-            this.ctx.fillRect(this.particleList[i].c_x, this.particleList[i].c_y, 2, 2);
+
+            if (r >= 0.8) {
+                this.ctx.fillStyle = `rgba(0, 180, 255)`;
+                // this.ctx.fillStyle = `rgba(230, 245, 5)`;
+            } else {}
+            this.ctx.fillRect(this.particleList[i].c_x, this.particleList[i].c_y, this.particleList[i].p_w, this.particleList[i].p_h);
+
         }
     }
     step() {
-        this.count++;
         this.drawImg();
+        this.count++;
         this.frameId = window.requestAnimationFrame(this.step.bind(this));
         if (this.count == this.animateTime) {
+            console.log(this.count);
             cancelAnimationFrame(this.frameId);
+            return
         }
 
     }
@@ -89,14 +99,17 @@ class CanvasImg {
 
 class Particle {
     constructor(x, y, cell_w, cell_h, cell_index, imageData, s_x, s_y, e_x, e_y) {
+        this.p_w = 1;
+        this.p_h = 1;
         this.s_x = s_x;
         this.s_y = s_y;
         this.c_x;
         this.c_y;
-        this.e_x = e_x;
-        this.e_y = e_y;
-        this.x = x * cell_w + (Math.random() - 0.5) * 20;
-        this.y = y * cell_h + (Math.random() - 0.5) * 20;
+        this.e_x = e_x + ((Math.random() - 0.5) * 10);
+        this.e_y = e_y + ((Math.random() - 0.5) * 10);
+        this.x = x * cell_w + ((Math.random() - 0.5) * 20);
+        this.y = y * cell_h + ((Math.random() - 0.5) * 20);
+        this.c_t = Math.floor((Math.random() - 0.5) * 200);
         this.style =
             `rgba(${imageData[cell_index]}, ${imageData[cell_index+1]}, ${imageData[cell_index+2]}, ${imageData[cell_index+3]})`
     }
